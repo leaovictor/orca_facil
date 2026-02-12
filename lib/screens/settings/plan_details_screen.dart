0c929624ae749d7b4dabea3faffe03fd2560c185
Victor Leão <leaovic@gmail.com>
2026-02-12 06:25:47 -0300

    chore: Remove unused `flutter/services.dart` import.

--- lib/screens/settings/plan_details_screen.dart
+++ lib/screens/settings/plan_details_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:go_router/go_router.dart';
